65c8ef783127b6763c703bfca03952cead926523
Ben Grosser <grosser@bengrosser.com>
2016-09-11 16:09:49 -0500

    removed unneeded code

--- allbrowsers/facebookdemetricator.user.js
+++ allbrowsers/facebookdemetricator.user.js
@@ -4663,38 +4663,6 @@ this.display)):(this.d.data.hide().remove(),a.replaceWith(this.d.orig))}else thi
 
 
 
-var whichBrowser = function() {
-    // Return cached result if avalible, else get result then cache it.
-    if (browser.prototype._cachedResult)
-        return browser.prototype._cachedResult;
-
-    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-
-    // At least Safari 3+: "[object HTMLElementConstructor]"
-    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-
-    // Chrome 1+
-    var isChrome = !!window.chrome && !isOpera;
-
-    // At least IE6
-    var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-    // Edge 20+
-    var isEdge = !isIE && !!window.StyleMedia;
-
-    return browser.prototype._cachedResult =
-        isOpera ? 'Opera' :
-        isFirefox ? 'Firefox' :
-        isSafari ? 'Safari' :
-        isChrome ? 'Chrome' :
-        isIE ? 'IE' :
-        isEdge ? 'Edge' :
-        "Don't know";
-};
 
 
 // launch with the main once the script loads
